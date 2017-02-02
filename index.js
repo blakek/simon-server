b60b71ad27583e646bb3f3b8249be30ee51892fa
Blake Knight <oss.ideas@gmail.com>
2017-02-01 21:59:32 -0600

    ⚡ rewrite server using socket.io

--- index.js
+++ index.js
@@ -1,51 +1,101 @@
-const wsServer = require('json-websocket-server')()
-const { addUser, getUserConnection, removeUser } = require('./users')
+const port = process.env.PORT || 3000
+const io = require('socket.io')(port)
 
-wsServer.on('join', handleUserJoin)
-wsServer.on('part', handleUserLeave)
-wsServer.on('send', handleSayMessage)
-wsServer.on('yell', handleBroadcastMessage)
-wsServer.on('disconnect', handleUserLeave)
-
-function handleBroadcastMessage(connection, userMessage) {
-  connection.send('not-yet-implemented')
+const messages = {
+  duplicateUsername: 'Username already being used; please choose another username.',
+  invalidGroupName: 'A valid group name is needed.',
+  invalidUsername: 'A valid username is needed.',
+  noUser: 'Could not find username for that group.'
 }
 
-function handleSayMessage(connection, userMessage) {
-  sayStuff(userMessage)
-    .then(() => connection.send('sent'))
-    .catch(error => {
-      const reason = error.message
-      connection.send('user-leave-error', { error, reason })
-    })
+const handleJoin = (socket) => ({ group = '', username = '' }) => {
+  if (!isValidName(group)) {
+    return socket.emit('simon-error', 'join', messages.invalidGroupName)
+  }
+
+  if (!isValidName(username)) {
+    return socket.emit('simon-error', 'join', messages.invalidUsername)
+  }
+
+  const groupUsers = getGroupUsers({ group, socket })
+
+  if (groupUsers.includes(username)) {
+    return socket.emit('simon-error', 'join', messages.duplicateUsername)
+  }
+
+  socket.username = username
+  socket.join(group)
+
+  socket.emit('joined', { group, username })
 }
 
-function handleUserJoin(connection, groupUser) {
-  addUser(connection, groupUser)
-    .then(() => {
-      connection.send('joined', groupUser)
-    })
-    .catch(error => {
-      const reason = error.message
-      connection.send('user-add-error', { error, reason })
-    })
+const handleList = (socket) => ({ group = '' }) => {
+  const users = getGroupUsers({ group, socket })
+  socket.emit('users', { group, users })
 }
 
-function handleUserLeave(connection, groupUser) {
-  removeUser({ connection, groupUser })
-    .then(() => {
-      connection.send('left', groupUser)
-    })
-    .catch(error => {
-      const reason = error.message
-      connection.send('user-leave-error', { error, reason })
-    })
-    .catch(() => { /* don't throw errors from trying to write to closed connection */ })
+const handlePart = (socket) => ({ group = '', username = '' }) => {
+  socket.leave(group)
 }
 
-function sayStuff({ to, from, text }) {
-  return getUserConnection({ group: to.group, username: to.username })
-    .then(destination => {
-      destination.send('say', { to, from, text })
-    })
+const handleSend = (socket) => ({ to = { group: '', username: '' }, text = '' }) => {
+  const { group, username } = to
+  const sendNotFound = () => socket.emit('simon-error', 'recipient-not-found', messages.noUser)
+  const groupSockets = getGroupConnections({ group, socket })
+
+  if (!groupSockets) {
+    return sendNotFound()
+  }
+
+  const toUser = groupSockets.find(socketId => io.sockets.connected[socketId].username === username)
+
+  if (!toUser) {
+    return sendNotFound()
+  }
+
+  socket.to(toUser).emit('say', { text: text })
 }
+
+const handleYell = (socket) => ({ to = { group: '' }, text = '' }) => {
+  const groups = Object.keys(socket.adapter.rooms)
+
+  if (groups.includes(to.group)) {
+    return socket.to(to.group).emit('say', { text: text })
+  }
+
+  socket.emit('simon-error', 'recipient-not-found', messages.noUser)
+}
+
+function getGroupConnections({ group = '', socket }) {
+  const room = socket.adapter.rooms[group]
+
+  if (typeof room !== 'object' || room.sockets == null) {
+    return
+  }
+
+  return Object.keys(room.sockets)
+}
+
+function getGroupUsers({ group = '', socket }) {
+  const groupSockets = getGroupConnections({ group, socket })
+
+  if (!groupSockets) {
+    return []
+  }
+
+  return groupSockets.map(socketId => io.sockets.connected[socketId].username)
+}
+
+function isValidName(name) {
+  return typeof name === 'string' && name.length > 0
+}
+
+io.on('connection', socket => {
+  socket.on('join', handleJoin(socket))
+  socket.on('list', handleList(socket))
+  socket.on('part', handlePart(socket))
+  socket.on('send', handleSend(socket))
+  socket.on('yell', handleYell(socket))
+})
+
+console.log(`starting on port ${port}`)
